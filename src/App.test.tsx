be4fd1de7a9800f4c377674afcf73daf381c802a
buinguyet <Buianhnguyet10a2@gmail.com>
2022-08-22 23:21:40 +0700

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import 'jest-canvas-mock';
 
 test('renders learn react link', () => {
   render(<App />);
